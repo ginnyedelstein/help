b534e67a712649d931ced17d4630e90b7a426ab1
Virginia Edelstein <ginny.edelstein@gmail.com>
2022-11-03 10:12:34 -0400

    Fix profile page

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -7,19 +7,16 @@ import Config from "../lib/Config";
 const Profile = ({ user }) => {
   const [userData, setUserData] = useState({});
   const userName = user.replace(/"/g, "");
-  const lnk = `${Config.apiUrl}/users/83a6b4bd-9975-42ba-987d-e12d031a80b0`;
+  const lnk = `${Config.apiUrl}/users/${userName}`;
   const fetchUser = async () => {
     try {
-      const res = await fetch(
-        `${Config.apiUrl}/users/83a6b4bd-9975-42ba-987d-e12d031a80b0`,
-        {
-          method: "GET",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-        }
-      );
+      const res = await fetch(`${Config.apiUrl}/users/${userName}`, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
       const resJson = await res.json();
       if (res.status === 200) {
         let json_data = resJson.body;
@@ -34,17 +31,17 @@ const Profile = ({ user }) => {
   useLayoutEffect(() => {
     fetchUser();
   }, []);
-  console.log(userData);
+  console.log(userData.firstName);
   return (
     <View style={styles.container}>
-      <Text>CURRENT USER: {user}</Text>
+      <Text>CURRENT USER: {userData.userId}</Text>
       <Text />
       <Text />
 
       <Text />
 
       <MaterialCommunityIcons name="account-circle" color={"grey"} size={100} />
-      {userData[0] ? <Text>{userData[0]["firstName"]}</Text> : <Text />}
+      {userData ? <Text>{userData.firstName}</Text> : <Text />}
 
       <View style={{ flexDirection: "row" }}>
         <MaterialCommunityIcons name="star" color={"grey"} size={30} />
@@ -53,14 +50,14 @@ const Profile = ({ user }) => {
       </View>
       <Text />
 
-      <Text>{userData[0] ? userData[0]["helpsGiven"] : "0"} helps given</Text>
+      <Text>{userData ? userData["helpsGiven"] : "0"} helps given</Text>
       <Text>
-        {userData[0] ? userData[0]["helpsReceived"] : "0"} helps received
+        {userData ? userData["helpsReceived"] : "0"} helps received
       </Text>
       <Text />
-      <Text>{userData[0] ? userData[0]["gender"] : ""}</Text>
-      <Text>{userData[0] ? userData[0]["address"] : ""}</Text>
-      <Text>{userData[0] ? userData[0]["birthdate"] : ""}</Text>
+      <Text>{userData ? userData["gender"] : ""}</Text>
+      <Text>{userData ? userData["address"] : ""}</Text>
+      <Text>{userData ? userData["birthdate"] : ""}</Text>
     </View>
   );
 };
